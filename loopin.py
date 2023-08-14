e618e4d8a7b3f967699a69706c106276250076c6
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-14 18:36:13 -0400

    move comments

--- loopin.py
+++ loopin.py
@@ -1,9 +1,7 @@
 print ("\n" "\n")
 
-
-languages = ['Swift', 'Python', 'Go', 'JavaScript']
-
 # # run a loop for each item of the list
+# languages = ['Swift', 'Python', 'Go', 'JavaScript']
 # for language in languages:
 #     print(language)
 
@@ -11,17 +9,17 @@ languages = ['Swift', 'Python', 'Go', 'JavaScript']
 
 
 ### FACTORIAL ### (Python does not have traditional for loops :(.))
-def factorial(x):
-    factor = 1
-    for num in range(1, x + 1):
-        factor *= num
-    return factor
+# def factorial(x):
+#     factor = 1
+#     for num in range(1, x + 1):
+#         factor *= num
+#     return factor
     
 
-one_hundred_twenty = factorial(5)
+# one_hundred_twenty = factorial(5)
 
-print(one_hundred_twenty)
-### FACTORIAL ###
+# print(one_hundred_twenty)
+
 
 
 
